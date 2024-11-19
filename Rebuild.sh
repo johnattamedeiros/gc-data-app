3091c27f3ccd7569963817b4afa8687f751fb35a
Johnatta Medeiros dos Santos <johnattamedeiros@gmail.com>
2024-11-19 17:00:56 -0300

    Adjust rebuild

--- Rebuild.sh
+++ Rebuild.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+# Interromper o script em caso de erro
+set -e
+
+# Atualizar o repositório
+echo "Atualizando o repositório com git pull..."
+git pull
+
 # Parar o contêiner, se estiver em execução
-docker stop gc_data_app
+echo "Parando o contêiner existente..."
+docker stop gc_data_app || true
 
 # Remover o contêiner
-docker rm gc_data_app
+echo "Removendo o contêiner existente..."
+docker rm gc_data_app || true
 
 # Remover a imagem
-docker rmi gc_data_app
+echo "Removendo a imagem existente..."
+docker rmi gc_data_app || true
 
 # Construir a nova imagem
+echo "Construindo a nova imagem..."
 docker build -t gc_data_app .
 
 # Rodar o contêiner
+echo "Iniciando o novo contêiner..."
 docker run -d --name gc_data_app -p 3000:3000 gc_data_app
 
 echo "Container 'gc_data_app' iniciado e disponível na porta 3000."
