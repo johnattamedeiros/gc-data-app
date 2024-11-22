13f175132ed19367ec77df9d6930d3b5acca0af3
Johnatta Medeiros dos Santos <johnattamedeiros@gmail.com>
2024-11-22 17:15:33 -0300

    test

--- services/PlayerService.js
+++ services/PlayerService.js
@@ -1,7 +1,9 @@
 const Player = require('../models/Player');
 const Match = require('../models/Match');
 const { sequelize } = require('../config/db');
+const axios = require('axios');
 require('dotenv').config();
+
 
 
 class PlayerService {
@@ -80,7 +82,7 @@ class PlayerService {
         try {
             const player = await Player.findByPk(playerId);
             if (player) {
-                player.stats = playerData.stat;
+                player.stats = playerData.stats;
                 await player.save();
             }
         } catch (error) {
@@ -133,7 +135,7 @@ class PlayerService {
                 headers: {
                     accept: "application/json, text/plain, */*",
                     "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-                    authorization:  process.env.TOKEN,
+                    authorization: process.env.TOKEN2,
                     referer: `https://gamersclub.com.br/player/${player.id}`,
                 },
                 method: "GET",
@@ -151,21 +153,33 @@ class PlayerService {
     };
     async fetchPlayerData(player) {
         try {
-            const response = await fetch(`https://gamersclub.com.br/api/box/init/${player.id}`, {
+            const response = await axios.get(`https://gamersclub.com.br/api/box/init/${player.id}`, {
                 headers: {
-                    accept: "application/json, text/plain, */*",
-                    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-                    authorization:  process.env.TOKEN,
-                    referer: `https://gamersclub.com.br/player/${player.id}`,
-                },
-                method: "GET",
+                    'accept': 'application/json, text/plain, */*',
+                    'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                    'authorization': process.env.TOKEN2,
+                    'cache-control': 'no-cache',
+                    'cookie': 'sib_cuid=0fc425dc-7346-47be-a44b-dfb115c5bb27; _fbp=fb.2.1723654395117.529201941752002953; _tt_enable_cookie=1; language=pt-br; _hjSessionUser_2263196=eyJpZCI6IjJiZmFlODE3LTY0YmMtNThlMC04M2Y5LTVjMTRkY2ViZDA4OSIsImNyZWF0ZWQiOjE3MjM2NTQ0ODkxMDYsImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_1963917=eyJpZCI6IjExMDdkZmNiLWY4ZGEtNWI5My04ZjM3LWJlNTc5NTYzNzAzYyIsImNyZWF0ZWQiOjE3MjM2NTQzOTUyMjQsImV4aXN0aW5nIjp0cnVlfQ==; 51QQyhcLyDRpqrY2Gh3vO=1; _ga_1WKB6YC210=deleted; SL_C_23361dd035530_SID={"a14d3638cda988422792e3613234743b983fdd9e":{"sessionId":"Fe32YPsJXXRplQFklqvNH","visitorId":"pznzLWZCfcjtC_JEGCkDB"}}; FCNEC=%5B%5B%22AKsRol_VW-uHcuSCb9G8DWrcjp48op6AEcuXdY3_9WBOTHNZMm0YW74WBF07pMaI1k9fOxgthi_owwHggutHOBw-AKW8bL5bfrYK7asxDbE2s71cX5JBneUEf0Rqy_R8bZLmXRDRzp7JFPbBBB-JMlZCnexyePOxIQ%3D%3D%22%5D%5D; _gcl_au=1.1.1203603058.1731437524; _ttp=0aueWHaVlm-oO12sh6qCS8kzFwK.tt.2; gclubsess=6a7ed54ae8c20e62726f67c2d7ac5b4249a3dbea; gcid:accessToken=O4naxwPP1kTx0RW_63rsdrBJDrOpJyZvVXZANvxsX7I.6nzJsjOiD-kQjCgGud5cB9UrtC1RX5BTUZqsCFm6FWA; _gid=GA1.3.1324656860.1732229445; _ce.clock_data=30676%2C200.207.65.31%2C1%2Cf51bb482c660d0eeadd1f058058a2b35%2CChrome%2CBR; cebs=1; _hjSession_2263196=eyJpZCI6IjA2OTgzYTUyLTA1ZjQtNDc0MC1iNDcwLTczNjJjMzhhN2NkOCIsImMiOjE3MzIzMDE3NjY5MjIsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=; __cf_bm=e96WDs3kcfJqyoPMlmhc2JCdul7DJPBoNI8Ck8FivOo-1732305935-1.0.1.1-uu0cKoNBuE6WfZ9RSxwmjtxxSzqau0_FOu6qVySSL3TSSeV1s5QSf4eTIadaDfOJVIa1mnHlH6dEgx6.AWqeVw; _gat_UA-64910362-1=1; _gat_UA-64910362-39=1; _gat_UA-187315934-3=1; _gat_UA-187315934-4=1; cf_clearance=xG1DwJCFb0DKxbQfQPGIlGATYNyNY07dyNTXpYpA2q4-1732305936-1.2.1.1-2s361qCg30eTDRYL4F_dmwNHm3eJdmqKduezV7RyAg6SnW.NzErWD9Y..PP_gdw2yhYli6Notc7I5J2TLW0cWP16VCHRQ_JDC34.YX4T.i7fp33kr5gFghoMih.CGnqdbd3wR76J4AIx.PhittKhXxE5RmPG2dNA8ws9BAwQ6PW4HAHb09YnqySxSINUBdotYXwWMU3YQBJMmIebUF9K50ZjREeUT_ZiaAqcvzhqzoDFq_g8ktehWx.5bCFaXNVcbxlVo9_rmnmoI9h4otklSH11pQkGjdJVUXFcNwagw5gxvK4ZkEZexEypWO9o_e6uw4scOdkGDWjbv_D74PESndIifNHWSBGZwxwL63WTbHSt_5qY38264q0ImEy0aZqjvzPXYD.c2E_F562qIfaaMZHctXTVekGsZL1kumkt0EP.xfhsdqFmWfx.yLGTJ0ZE',
+                    'pragma': 'no-cache',
+                    'priority': 'u=1, i',
+                    'referer': 'https://gamersclub.com.br/player/2359599',
+                    'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+                    'sec-ch-ua-arch': '"x86"',
+                    'sec-ch-ua-bitness': '"64"',
+                    'sec-ch-ua-full-version': '"131.0.6778.86"',
+                    'sec-ch-ua-full-version-list': '"Google Chrome";v="131.0.6778.86", "Chromium";v="131.0.6778.86", "Not_A Brand";v="24.0.0.0"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-model': '""',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'sec-ch-ua-platform-version': '"10.0.0"',
+                    'sec-fetch-dest': 'empty',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-site': 'same-origin',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+                }
             });
 
-            if (!response.ok) {
-                throw new Error(`[Player Service] Gamersclub API not responding for player ID: ${player.id}`);
-            }
-
-            return await response.json();
+            return response.data;
         } catch (error) {
             console.error(`[Player Service] Error fetching data for player ID: ${player.id}`, error);
             throw error;
@@ -182,11 +196,13 @@ class PlayerService {
         }
 
         console.log(`[Player Service] Fetching data for player stats ID: ${player.id}`);
-        const playerStats = await this.fetchPlayerStats(player);
-        await this.updatePlayerStats(player.id, playerStats);
+        //const playerStats = await this.fetchPlayerStats(player);
+        //await this.updatePlayerStats(player.id, playerStats);
+        await this.updatePlayerStats(player.id, playerData.stats);
+        
         console.log(`[Player Service] Fetched all data for player stats ID: ${player.id}`);
     }
-    async  getPlayerById(id) {
+    async getPlayerById(id) {
         return await Player.findOne({
             where: {
                 id: id,
